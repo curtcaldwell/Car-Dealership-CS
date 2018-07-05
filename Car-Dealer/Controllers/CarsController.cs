9cd4d31984a181cfd64174f5025c4c1484e7f8fa
Curt Caldwell and Eliot Charette <curt@>
2018-07-05 14:36:26 -0700

    Add post form

--- Car-Dealer/Controllers/CarsController.cs
+++ Car-Dealer/Controllers/CarsController.cs
@@ -9,14 +9,22 @@ namespace CarDealer.Controllers
     [HttpGet("/Cars")]
     public ActionResult Car()
     {
-      Cars newCars = new Cars(Request.Query["car-make"]);
-      newCars.Save();
+      // Cars newCars = new Cars(Request.Query["car-make"]);
+      // newCars.Save();
       return View(Cars.GetAll());
+      // return View();
     }
     [HttpGet("/Cars/new")]
     public ActionResult Form()
     {
       return View();
+    }
+    [HttpPost("/cars")]
+    public ActionResult Create()
+    {
+      Cars newCars = new Cars(Request.Form["car-make"]);
+      newCars.Save();
+      return View("Car", Cars.GetAll());
     }
     [HttpGet("/Cars/Details")]
     public ActionResult Details()
